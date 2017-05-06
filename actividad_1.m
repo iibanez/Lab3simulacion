b89f57d617ea42639f9b318bfc6496b93ea22cd9
Ignacio Ibañez <ignacio.ibanez@usach.cl>
2017-05-06 20:10:43 -0300

    solucionado tema de escalas

--- actividad_1.m
+++ actividad_1.m
@@ -10,42 +10,44 @@ numc = [5 18];
 denc = [34 -6 25];
 
 %se obtiene la respuesta frente a un escalon del sistema continuo
-[y,t] = step(sist); 
+t=0:0.01:50;
+[y,t] = step(sist, t); 
 createfigure1(t,y,'Sistema continuo', 1);
 pause;
-clf();
+%clf();
 
 %transformar a forma discreta
 [numd1, dend1] = c2dm(numc, denc, 0.3, 'zoh');
 
 %construir los puntos
-[y1] = dstep(numd1, dend1, 6000+1);
-x1 = 0:0.05:0.05*6000;
+[y1] = dstep(numd1, dend1, 166+1);
+x1 = 0:0.3:0.3*166;
 
 %graficar sistema discreto 1
 createfigure1(x1,y1,'Sistema discreto 1 muestreo = 0.3', 2);
 pause;
-clf();
+%clf();
 
 %transformar a forma discreta
 [numd2, dend2] = c2dm(numc, denc, 0.05, 'zoh');
 
 %construir los puntos
-[y1] = dstep(numd2, dend2, 6000+1);
-x1 = 0:0.05:0.05*6000;
+[y1] = dstep(numd2, dend2, 1000+1);
+x1 = 0:0.05:0.05*1000;
 
 %graficar sistema discreto 1
 createfigure1(x1,y1,'Sistema discreto 2 muestreo = 0.05', 2);
 pause;
-clf();
+%clf();
 
 
 %transfromar de discreta a continua
-[numc1, denc1] = d2cm(numd2, dend2, 0.08, 'zoh');
+[numc1, denc1] = d2cm(numd1, dend1, 0.08, 'zoh');
 
 %graficar sistema discreto 1
-sist = tf(numc1, denc1);
-[y,t] = step(sist); 
-createfigure1(t,y,'Sistema continuo muestreo = 0.05', 1);
+sistc = tf(numc1, denc1);
+t=0:0.01:50;
+[y,t] = step(sistc,t); 
+createfigure1(t,y,'Sistema continuo muestreo = 0.08', 1);
 pause;
-clf();
+%clf();
